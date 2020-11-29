884fde1c4b42ddb88cc7d1e3bb22158449bce77b
Richard Manques <manques11@gmail.com>
2020-11-29 17:21:15 -0300

    login, sign in, misdenuncias

---
+++ app/src/main/java/com/example/talcawarningsapp/adapter/AdapterDenuncia.java
@@ -0,0 +1,58 @@
+package com.example.talcawarningsapp.adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.talcawarningsapp.R;
+import com.example.talcawarningsapp.model.Denuncia;
+
+import java.util.List;
+
+public class AdapterDenuncia extends RecyclerView.Adapter<AdapterDenuncia.DenunciHolder> {
+    private Activity activity;
+    private int layout;
+    private List<Denuncia> list;
+
+    public AdapterDenuncia(Activity activity, int layout, List<Denuncia> denuncia) {
+        this.activity = activity;
+        this.layout = layout;
+        this.list = denuncia;
+    }
+
+    @NonNull
+    @Override
+    public DenunciHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(layout,parent,false);
+        return new DenunciHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DenunciHolder holder, int position) {
+        Denuncia denuncia = list.get(position);
+        holder.titulo.setText(denuncia.getTitulo());
+        holder.direccion.setText(denuncia.getDireccion());
+        holder.id = denuncia.getId();
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class DenunciHolder extends RecyclerView.ViewHolder{
+        TextView titulo, direccion;
+        String id;
+
+        public DenunciHolder(@NonNull View itemView) {
+            super(itemView);
+            titulo =  itemView.findViewById(R.id.item_denuncia_title);
+            direccion = itemView.findViewById(R.id.item_denuncia_direccion);
+        }
+    }
+}
